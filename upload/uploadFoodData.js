503201de5662f07ce226331a75aa48888a5036dd
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-06 02:55:23 +0700

    can upload dummy data but just one by one

--- upload/uploadFoodData.js
+++ upload/uploadFoodData.js
@@ -21,6 +21,8 @@ fs.readdir(directoryPath, function (err, files) {
     var lastDotIndex = file.lastIndexOf(".");
     var dataMakanan = require("./data/" + file);
 
+    const foodIds = new Set(); // Objek Set untuk menyimpan food_id yang unik
+
     dataMakanan.forEach(function (foodData) {
       const foodName = Object.keys(foodData)[0];
       const foodCollection = db.collection("foods");
@@ -30,24 +32,31 @@ fs.readdir(directoryPath, function (err, files) {
 
       if (Array.isArray(foods)) {
         foods.forEach(function (food) {
-          const foodObj = {
-            food_description: food.food_description,
-            food_id: food.food_id,
-            food_name: food.food_name,
-            food_type: food.food_type,
-            food_url: food.food_url,
-          }; 
+          if (!foodIds.has(food.food_id)) {
+            // Memeriksa apakah food_id sudah ada dalam Set
+            foodIds.add(food.food_id); // Menambahkan food_id ke Set
 
-          foodDocRef
-            .collection("food")
-            .doc(food.food_id)
-            .set(foodObj)
-            .then(function () {
-              console.log("Document written");
-            })
-            .catch(function (error) {
-              console.error("Error adding document: ", error);
-            });
+            const foodObj = {
+              food_description: food.food_description,
+              food_id: food.food_id,
+              food_name: food.food_name,
+              food_type: food.food_type,
+              food_url: food.food_url,
+            };
+
+            foodDocRef
+              .collection("food")
+              .doc(food.food_id)
+              .set(foodObj)
+              .then(function () {
+                console.log("Document written");
+              })
+              .catch(function (error) {
+                console.error("Error adding document: ", error);
+              });
+          } else {
+            console.error("Duplicate food_id found: " + food.food_id);
+          }
         });
       } else {
         console.error("Invalid data format for foods in file: " + file);
